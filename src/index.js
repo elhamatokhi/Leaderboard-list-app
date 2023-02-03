083c412f3ccf6d7c48e98668334db2d06bf869fc
elhamatokhi <tokhielhama30@gmail.com>
2023-02-04 03:46:40 +0430

    Add post and get functions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-import './style.css';
+import { post ,get} from "./modules/API";
+import "./style.css";
+
+const name = document.querySelector(".name");
+const score = document.querySelector(".score");
+const btn = document.querySelector(".btn");
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+  post(name.value,score.value)
+});
+
+btn.addEventListener("click", get);
---
+++ src/modules/API.js
@@ -0,0 +1,26 @@
+export const post = async (name, score) => {
+  const res = await fetch(
+    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/4dIVkemOTTVg2fUdz/scores/",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user: name,
+        score: score,
+      }),
+    }
+  );
+  const data = await res.json();
+};
+export const get = async () => {
+  const res = await fetch(
+    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/4dIVkemOTTVg2fUdz/scores/"
+  );
+  const data = await res.json();
+  data.result.forEach((user) => {
+    const html = `<li>${user.user}: ${user.score}</li>`;
+    document.querySelector("ul").insertAdjacentHTML("beforeend", html);
+  });
+};
